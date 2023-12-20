7060ed67eea1f1eab3922ebb6e62fd1fd485ceda
Abdullah Madi <abodmadi2040@gmail.com>
2023-12-21 00:14:32 +0200

    Illness diagnosis with PyKE

--- BMIN520.py
+++ BMIN520.py
@@ -1,7 +1,7 @@
 import sys
 # The following command points your notebook to the location of a folder outside your working directory that you want to import.
 #sys.path.append('C:/Users/abdullah/Desktop/pyKE_Program/pyke-1.1.1/examples/simple_bc_all')
-sys.path.append('E:/MY_Projects/PyKE_Project/simple_bc_all')
+sys.path.append('E:/My_Projects/PyKE_Project/pyke-1.1.1/examples/simple_bc_all')
 #E:\MY_Projects\PyKE_Project\simple_bc_all
 #sys.path.append('yourpath')
 import driver_simple
